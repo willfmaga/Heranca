c56b3314271c5d131a61cc5552f14d4e32e34e52
Mario Torres <71908935+MarioTorres09032018@users.noreply.github.com>
2020-10-27 21:34:51 -0300

    27/10/2020_Upando_Cargo

--- Pessoa/Cargo.cs
+++ Pessoa/Cargo.cs
@@ -1,27 +1,45 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
+using Newtonsoft.Json;
+using System.IO;
+using System.Text.Json.Serialization;
 
 namespace Cadastro
 {
     public class Cargo
     {
+        const string PATHFILE = @"c:\CADASTROFUNCIONARIO\CadastroFuncionario.txt";
+        const string PASTA = @"c:\CADASTROFUNCIONARIO";
         public Cargo(string nome, string descricao, decimal salario)
         {
             this.Nome = nome;
             this.Descricao = descricao;
             this.Salario = salario;
+           
         }
 
         public string Nome { get; private set; }
-
         public string Descricao { get; private set; }
-
         public decimal Salario { get; private set; }
 
         public virtual decimal CalculaSalario()
         {
             return Salario;
+
+        }
+        public void Gravar()
+        {
+            if (!Directory.Exists(PASTA))
+            {
+                Directory.CreateDirectory(PASTA);
+            }
+
+            Console.WriteLine(File.Exists(PATHFILE));
+            File.WriteAllText(PATHFILE, "CadastroFuncionario");
+            Console.WriteLine(File.Exists(PATHFILE));
+            
+
+            //salva em um arquivo de txt
+            File.WriteAllText(PATHFILE, JsonConvert.SerializeObject(this) + "\n");
         }
     }
 }
