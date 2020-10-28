e456ddd86c55803043993e4a6a5bae48927727f2
Mario Torres <71908935+MarioTorres09032018@users.noreply.github.com>
2020-10-27 21:23:55 -0300

    27/10/2020_Upando_AnalistaTI_Completo

--- Pessoa/AnalistaTI.cs
+++ Pessoa/AnalistaTI.cs
@@ -7,17 +7,20 @@ namespace Cadastro
     public class AnalistaTI : Cargo
     {
 
-        public AnalistaTI(string nome, string descricao, decimal salario, int horasExtras, int diasTrabalhados) : base(nome, descricao, salario)
+        public AnalistaTI(string nome, string descricao, decimal salario, int horasExtras, int diasTrabalhados) 
+            : base(nome, descricao, salario)
+            
         {
             HorasExtras = horasExtras;
             DiasTrabalhados = diasTrabalhados;
             Salario = salario;
-
+           
         }
-
+        
         public int HorasExtras { get; private set; }
         public int DiasTrabalhados { get; private set; }
         public new decimal Salario { get; private set; }
+       
 
         public override decimal CalculaSalario()
         {
