ea599fb6dd4290756107db4f0600c6c3df8e29b7
Mario Torres <71908935+MarioTorres09032018@users.noreply.github.com>
2020-11-24 17:27:54 +0000

    24/11/2020_Editando_Convert_DOCs_

--- Pessoa/PessoaFisica.cs
+++ Pessoa/PessoaFisica.cs
@@ -6,16 +6,72 @@ namespace Aula
 {
     public class PessoaFisica : Pessoa
     {
-        public PessoaFisica(string nome, DateTime dataNascimento,
-                            string sobrenome, string cpf, string rg, string cordosolhos)
-                            : base(nome, dataNascimento, sobrenome, cordosolhos)
+        public PessoaFisica(string nome, string sobrenome, string cpf, string rg, string nacionalidade, DateTime dataNascimento)
+
+                            : base(nome, sobrenome, nacionalidade, dataNascimento)
         {
+            Nome = nome;
+            Sobrenome = sobrenome;
             CPF = cpf;
             RG = rg;
+
+
         }
+        public string Nome { get; private set; }
+
+        public string Sobrenome { get; private set; }
+
         public string CPF { get; private set; }
 
         public string RG { get; private set; }
 
+
+        private Convesao(string CPF, string RG);
+
+
     }
+
+    public class TentativaConversao
+    {
+        public void Conversao()
+        {
+            int num=0;
+            string CPF = "0123456789x";
+            string RG = "0123456789x";
+            bool Sucesso;
+            Sucesso = int.TryParse(CPF, out num);
+
+            
+
+            if (Sucesso)
+            {
+                Console.WriteLine("Conversão Bem Sucedida! O Valor do némero é" + num);
+            }
+            else
+            {
+                Console.WriteLine("A Conversão Falhou!");
+            }
+
+            Sucesso = int.TryParse(RG, out num);
+
+            if(Sucesso)
+            {
+                Console.WriteLine("Conversão Bem Sucedida! O Valor do némero é" + num);
+            }
+            else
+            {
+                Console.WriteLine("A Conversão Falhou!");
+            }
+        }
+    }
+
 }
+
+    
+
+
+
+    
+   
+
+
