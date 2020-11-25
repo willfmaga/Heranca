ba9071662b1ad3f8e47bc1746ac66fe298489265
Mario Torres <71908935+MarioTorres09032018@users.noreply.github.com>
2020-11-24 23:15:44 -0300

    24/11/2020_Editando_Convert_DOCs_

--- Pessoa/PessoaFisica.cs
+++ Pessoa/PessoaFisica.cs
@@ -35,7 +35,7 @@ namespace Aula
             string RG = "0123456789x";
             bool Sucesso;
 
-            PessoaFisica RegGeral = new PessoaFisica(nome,sobrenome,cpf,rg,nacionalidade,dataNascimento); 
+            PessoaFisica RegGeral = new PessoaFisica(nome, sobrenome, cpf, rg, nacionalidade, dataNascimento); 
             PessoaFisica RegFederal = new PessoaFisica(nome, sobrenome, cpf, rg, nacionalidade, dataNascimento);
 
             Sucesso = Int64.TryParse(CPF, out num);
