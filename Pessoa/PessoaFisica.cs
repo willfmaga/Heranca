26c1e20f05d0573653c3ac7fd5f8d015c6625210
Mario Torres <71908935+MarioTorres09032018@users.noreply.github.com>
2020-11-26 16:06:58 -0300

    26/11/2020_Editando_Convert_DOCs_

--- Pessoa/PessoaFisica.cs
+++ Pessoa/PessoaFisica.cs
@@ -8,58 +8,23 @@ namespace Aula
     {
 
         public PessoaFisica(string nome, string sobrenome, string cpf, string rg, string nacionalidade, DateTime dataNascimento)
-                              
-                            : base(nome, sobrenome,nacionalidade, dataNascimento)
+
+                            : base(nome, sobrenome, nacionalidade, dataNascimento)
         {
             Nome = nome;
             Sobrenome = sobrenome;
             CPF = cpf;
             RG = rg;
+
         }
+
         public string Nome { get; private set; }
 
         public string Sobrenome { get; private set; }
 
         public string CPF { get; private set; }
-
+            
         public string RG { get; private set; }
 
     }
-
-    public class TentativaConversao
-    {
-        public void Main(string nome, string sobrenome, string cpf, string rg, string nacionalidade, DateTime dataNascimento)
-        {
-            Int64 num = 0;
-            string CPF = "0123456789x";
-            string RG = "0123456789x";
-            bool Sucesso;
-
-            PessoaFisica RegGeral = new PessoaFisica(nome, sobrenome, cpf, rg, nacionalidade, dataNascimento); 
-            PessoaFisica RegFederal = new PessoaFisica(nome, sobrenome, cpf, rg, nacionalidade, dataNascimento);
-
-            Sucesso = Int64.TryParse(CPF, out num);
-
-            if (Sucesso)
-            {
-                Console.WriteLine("Conversão Bem Sucedida! O Valor do número é" + num);
-            }
-            else
-            {
-                Console.WriteLine("A Conversão Falhou!");
-            }
-
-            Sucesso = Int64.TryParse(RG, out num);
-
-            if (Sucesso)
-            {
-                Console.WriteLine("Conversão Bem Sucedida! O Valor do número é" + num);
-            }
-            else
-            {
-                Console.WriteLine("A Conversão Falhou!");
-            }
-        }
-    }
-
 }
---
+++ Pessoa/TentativaConversao.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using System.Text.Json.Serialization;
+
+namespace Aula
+
+{
+    class TentativaConversao : PessoaFisica
+    {
+
+        public TentativaConversao(string nome, string sobrenome, string cpf, string rg, string cnpj, string nacionalidade, DateTime dataNascimento, Int64 num)
+                             : base(nome, sobrenome, cpf, rg, nacionalidade, dataNascimento)
+        {
+            
+            Nome = nome;
+            Sobrenome = sobrenome;
+            CPF = cpf;
+            RG = rg;
+            Num = num;
+        }
+
+        public string Nome { get; private set; }
+
+        public string Sobrenome { get; private set; }
+
+        public string CPF { get; private set; }
+
+        public string RG { get; private set; }
+
+
+        public Int64 Num { get; private set; }
+
+
+        public void ConvertDOCS(string nome, string sobrenome, string cpf, string rg, string cnpj, string nacionalidade, DateTime dataNascimento)
+
+        {
+            Int64 num = 0;
+            bool Sucesso = false;
+            string CPF = "0123456789x";
+            string RG = "0123456789x";
+
+            this.Num = num;
+            this.CPF = cpf;
+            this.RG = rg;
+
+            PessoaFisica RegGeral = new PessoaFisica(nome, sobrenome, cpf, rg, nacionalidade, dataNascimento);
+            PessoaFisica RegFederal = new PessoaFisica(nome, sobrenome, cpf, rg, nacionalidade, dataNascimento);
+
+
+            Sucesso = Int64.TryParse(CPF, out num);
+
+            if (Sucesso)
+            {
+                Console.WriteLine("Conversão Bem Sucedida! O Valor do número é" + num);
+            }
+            else
+            {
+                Console.WriteLine("A Conversão Falhou!");
+            }
+
+            Sucesso = Int64.TryParse(RG, out num);
+
+            if (Sucesso)
+            {
+                Console.WriteLine("Conversão Bem Sucedida! O Valor do número é" + num);
+            }
+            else
+            {
+                Console.WriteLine("A Conversão Falhou!");
+            }
+
+        }
+    }
+}
+
+    
+        
+    
+
